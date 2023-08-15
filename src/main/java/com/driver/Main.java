691d370b9a1761ed1018e62528e167ab0130605e
nsp1919 <110881782+nsp1919@users.noreply.github.com>
2023-08-15 20:15:50 +0530

    A

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -4,7 +4,7 @@ package com.driver;
 public class Main {
     public static class A{
         public String meth(){
-            return "Invoking method from class a";
+            return "Invoking method from class A";
         }
     }
     public static class B extends A{
